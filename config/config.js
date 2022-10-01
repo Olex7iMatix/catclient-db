44be27c82c355e6c8ca9341e5d0a0052dc33bf19
Olex7iMatix <86416834+Olex7iMatix@users.noreply.github.com>
2022-10-01 21:31:13 +0200

    commit

---
+++ config/config.js
@@ -0,0 +1,15 @@
+//[major].[minor].[patch]-[release/beta]
+module.exports = {
+    //DONT CHANGE ME
+    version: '0.0.11-Alpha',
+    
+    //Change anything below this line
+    client_secret: 'CatClient',
+    port: 81,
+    
+    clientSettings: {
+        isWhitelisted: false,
+        latestVersion: "0.0.11"
+        /*Add anything here you want to be included in /api/globalSettings*/
+    }
+}
